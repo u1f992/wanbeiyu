e0bb68e588687676108577988ead40eac5f58431
Koutaro Mukai <mukai.k1011k@outlook.jp>
2025-01-16 19:04:38 +0900

    Create LICENSE

--- firmware/firmware.cydsn/main.c
+++ firmware/firmware.cydsn/main.c
@@ -1,28 +1,32 @@
-/* ========================================
+/*
+ * Copyright (C) 2025  Koutaro Mukai
  *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * ========================================
-*/
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "project.h"
 
 #include <wanbeiyu.h>
 
-int main(void)
-{
-    CyGlobalIntEnable; /* Enable global interrupts. */
+int main(void) {
+  CyGlobalIntEnable; /* Enable global interrupts. */
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+  /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
-    for(;;)
-    {
-        /* Place your application code here. */
-    }
+  for (;;) {
+    /* Place your application code here. */
+  }
 }
 
 /* [] END OF FILE */
